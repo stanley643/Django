9a956938b9bb1306365a22e114f420a443e8eff0
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-06 18:20:30 +0300

    app name

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'polls'
+#app_name = 'polls'
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path('<int:question_id>/', views.DetailView.as_view(), name='detail'),
